23b20344c0165dfcab56eedb58bf5aacf82e9e69
gcg <georg.ganzenmueller@inatech.uni-freiburg.de>
2025-01-25 14:29:25 +0100

    bug fixes

--- Symmpact/CFC_analysis.py
+++ Symmpact/CFC_analysis.py
@@ -2,7 +2,7 @@
 # @Author: Georg C. Ganzenmueller, Albert-Ludwigs Universitaet Freiburg, Germany
 # @Date:   2024-12-09 08:20:48
 # @Last Modified by:   Georg C. Ganzenmueller, Albert-Ludwigs Universitaet Freiburg, Germany
-# @Last Modified time: 2025-01-24 22:51:44
+# @Last Modified time: 2025-01-25 14:28:30
 
 """
 Apply the wave separation technique of 
@@ -25,6 +25,7 @@ class solveCFC:
         self.A_bar = 0.25 * np.pi * 40**2
         self.c0 = np.sqrt(self.E_bar/self.rho)
         self.L0 = 10.0 # specimen length
+        self.alpha = 0.0
 
         self.specimen_diameter = 10.0
         self.JC_A = 0.08 # Johnson-Cook A
@@ -33,18 +34,20 @@ class solveCFC:
         
 
         self.read_file(filename)
-        self.read_specimen()
+        #self.read_specimen()
         
-        if False:
+        if True:
             self.calculate_F_G_FrequencyDomain()
-            self.resolveAtDistanceFrequencyDomain(133.0, -133.0)
+            self.resolveAtDistanceFrequencyDomain(130.0, -130.0)
         else:
             self.calculate_F_G_TimeDomain()
-            self.resolveAtDistanceTimeDomain(133.0, -133.0)
+            self.resolveAtDistanceTimeDomain(130.0, -130.0)
         
+
+        self.plot_AB()
         #self.plot_FG()
         #self.plot_strain()
-        self.plot_specimen()
+        #self.plot_specimen()
         #self.plot()
 
     def resolveAtDistanceTimeDomain(self, dA, dB):
@@ -94,13 +97,23 @@ class solveCFC:
         self.FA_shifted = np.fft.irfft(FAws)
         self.GA_shifted = np.fft.irfft(GAws)
 
+        # after shifting, we can have lost the DC offset, initial value at t=0 is zero. recover this.
+        #self.FA_shifted -= self.FA_shifted[0]
+        #self.GA_shifted -= self.GA_shifted[0]
+
+
+
         Pw_shifted    = -self.rho * (self.w**2 / (self.gamma**2)) * (FAws    + GAws)    * self.A_bar
         Pw_shifted[0] =  self.rho * (self.c0**2)                  * (FAws[0] + GAws[0]) * self.A_bar
         self.PA_shifted = np.fft.irfft(Pw_shifted)
+        
+        print(f"offset in PA_shifted is:", self.PA_shifted[0])
+        #self.PA_shifted -= self.PA_shifted[0]
 
         vw_shifted = 1.0j * (self.w / self.gamma) * (FAws - GAws)
         vw_shifted[0] = self.c0 * (FAws[0] - GAws[0])
         self.vA_shifted = -np.fft.irfft(vw_shifted)
+        #self.vA_shifted 
 
         # do strain gauge B
         # shift F (e -gamma d) and G (e gamma d) in frequency space
@@ -118,7 +131,7 @@ class solveCFC:
         vw_shifted[0] = self.c0 * (FBws[0] - GBws[0])
         self.vB_shifted = -np.fft.irfft(vw_shifted)
 
-        print("gamma:", self.gamma)
+        #print("gamma:", self.gamma)
 
     def calculate_F_G_FrequencyDomain(self):
         """
@@ -137,6 +150,9 @@ class solveCFC:
         self.GAw[0] = 0.5 * (EPSA[0] + 1.0j * (1.0j/self.c0) * VA[0])  # need to handle w=0 separately, otherwise division by zero
         self.GA = np.fft.irfft(self.GAw)
 
+        
+
+
         # strain gauge B
         EPSB = np.fft.rfft(self.epsB)# * dt
         VB = np.fft.rfft(self.velB)# * dt
@@ -148,6 +164,19 @@ class solveCFC:
         self.GBw =    0.5 * (EPSB    + 1.0j * (self.gamma/self.w)      * VB)
         self.GBw[0] = 0.5 * (EPSB[0] + 1.0j * (1.0j/self.c0) * VB[0])  # need to handle w=0 separately, otherwise division by zero
         self.GB = np.fft.irfft(self.GBw)
+
+
+        # calculate Time Domain F, G for debugging
+        #self.FATD = 0.5 * (self.epsA + self.velA / self.c0)
+        #self.GATD = 0.5 * (self.epsA - self.velA / self.c0)
+
+        #plt.plot(self.t, self.FA, label="FA Fourier")
+        #plt.plot(self.t, self.GA, label="GA Fourier")
+        #plt.plot(self.t, self.FATD, label="FA TD")
+        #plt.plot(self.t, self.GATD, label="GA TD")
+        #plt.legend()
+        #plt.show()
+        #sys.exit()
 
 
 
@@ -175,7 +204,13 @@ class solveCFC:
         read a datafile suitable for CFC analysis.
         time, strain, velocity
         """
-        zeroPad = False
+
+        def mirror(seq):
+            mirrored = seq[::-1]
+            return np.concatenate((seq, mirrored))
+        
+
+        zeroPad = True
         low_pass_filtering = True
 
         data = np.genfromtxt(filename)
@@ -188,6 +223,21 @@ class solveCFC:
         N = len(t)
         print("read file [%s], dt=%g, Nrec=%d " % (filename, self.dt, N))
 
+        #read specimen strain and force from simulation
+        data = np.genfromtxt("specimen.dat")
+        specimen_stress = data[:,1]
+        specimen_strain = data[:,2]
+        assert len(specimen_stress) == N
+
+        #epsA = mirror(epsA)
+        #epsB = mirror(epsB)
+        #velA = mirror(velA)
+        #velB = mirror(velB)
+        #specimen_stress = mirror(specimen_stress)
+        #specimen_strain = mirror(specimen_strain)
+        #N = len(epsA)
+        #t = np.arange(N) * self.dt
+
         #if len(t) % 2 != 0:
         #    t = t[:-1]
         #    velA = velA[:-1]
@@ -197,49 +247,88 @@ class solveCFC:
 
         print("Nrec", len(t))
 
-        if zeroPad == True:
-            n = np.ceil(np.log(N)/np.log(2)) + 1; 
-            m = int(2**(n))
-            print("next power of 2 = %d, %d" %(n, m))
+        
 
-            diff= m - N + 1
-            velA = np.pad(velA, (0, diff), 'constant', constant_values=0.0)
-            epsA = np.pad(epsA, (0, diff), 'constant', constant_values=0.0)
-            t = np.arange(m) * self.dt
-            self.dt = t[1] - t[0]
+        if zeroPad == True:
+            #n = np.ceil(np.log(N)/np.log(2)) + 1; 
+            #m = int(2**(n))
+            #print("next power of 2 = %d, %d" %(n, m))
+
+            #diff= m - N + 1
+            diff = 1000
+
+            
+            velA = np.pad(velA, (diff, diff), 'constant', constant_values=velA[0])
+            epsA = np.pad(epsA, (diff, diff), 'constant', constant_values=epsA[0])
+            velB = np.pad(velB, (diff, diff), 'constant', constant_values=velB[0])
+            epsB = np.pad(epsB, (diff, diff), 'constant', constant_values=epsB[0])
+            specimen_strain = np.pad(specimen_strain, (diff, diff), 'constant', constant_values=specimen_strain[0])
+            specimen_stress = np.pad(specimen_stress, (diff, diff), 'constant', constant_values=specimen_stress[0])
+
+            m = len(velA)
+            t = np.arange(m) * self.dt - diff * self.dt
             print("padded with zeros, new x-axis extends from %g to %g, dt=%g" % (t[0], t[-1], self.dt))
             print("new length of x:",len(t))
             print("new length of y:",len(velA))
             #assert len(y) == m
 
         if low_pass_filtering == True:
-            plt.plot(t, velA, label="original")
-
+            #plt.plot(t, epsA, label="original")
             fs = 1. / self.dt
             fc = 50.0  # 
             omega = fc / (fs / 2) # Normalize the frequency
             b, a = signal.butter(1, omega, 'low')
             velA = signal.filtfilt(b, a, velA)
             epsA = signal.filtfilt(b, a, epsA)
+            velB = signal.filtfilt(b, a, velB)
+            epsB = signal.filtfilt(b, a, epsB)
 
-            plt.plot(t, velA, label="filtered")
-            plt.show()
+            #plt.plot(t, epsA, label="filtered")
+            #plt.legend()
+            #plt.show()
 
-        def mirror(seq):
-            mirrored = seq[::-1]
-            return np.concatenate((seq, mirrored))
+        
 
         self.t = t
         self.velA = velA
         self.epsA = epsA
         self.velB = velB
         self.epsB = epsB
+        self.specimen_strain = specimen_strain
+        self.specimen_stress = specimen_stress
 
         self.w = 2*np.pi * np.fft.rfftfreq(len(self.t), d=self.dt)
-        self.alpha = 0.0
         self.gamma = self.alpha + 1.0j * self.w / self.c0
-        self.gamma[0] = self.gamma[1]
+
+        # check if there are any poles in gamma
+        for i in range(len(self.gamma)):
+            if abs(self.gamma[i]) < 1.0e-8:
+                print(f"pole in gamma at index {i}, value is {self.gamma[i]}")
+
+        #sys.exit()
+
+
+        #self.gamma[0] = self.gamma[1]
         print("frequencies:", self.w)
+
+    def plot_AB(self):
+        """
+        plot the strain gauge signals at A and B
+        """
+
+        plt.plot(self.t, -self.rho * self.c0**2 * self.epsA * self.A_bar, label="force A")
+        plt.plot(self.t, -self.rho * self.c0**2 * self.epsB * self.A_bar, label="force B")
+        plt.plot(self.t, -self.PA_shifted, "c--", label="force A shifted")
+        plt.plot(self.t, -self.PB_shifted, "r--", label="force B shifted")
+
+        specimen_area = 0.25 * np.pi * self.specimen_diameter**2
+        specimen_force = -self.specimen_stress * specimen_area
+        plt.plot(self.t, specimen_force, "g--", label="specimen")
+        #plt.xlim(-0.20, 0.25)
+        #plt.ylim(-1, None)
+        plt.legend()
+        plt.grid()
+        plt.show()
 
     def plot_FG(self):
         """
@@ -311,9 +400,9 @@ class solveCFC:
         
         specimen_area = 0.25 * np.pi * self.specimen_diameter**2
         
-        plt.plot(self.t, self.specimen_stress * specimen_area, label="specimen force")
-        plt.plot(self.t, self.PA_shifted, label="force A shifted")
-        plt.plot(self.t, self.PB_shifted, label="force B shifted")
+        plt.plot(self.t, -self.specimen_stress * specimen_area, label="specimen force")
+        #plt.plot(self.t, self.PA_shifted, label="force A shifted")
+        #plt.plot(self.t, self.PB_shifted, label="force B shifted")
 
         force_avg = 0.5 * (self.PA_shifted + self.PB_shifted)
 
@@ -322,9 +411,11 @@ class solveCFC:
         tau = 0.0035 # step time in rise of signal
         nsmooth = int(tau / dt)
 
-        force_avg = bn.move_mean(force_avg, nsmooth)
+        #force_avg = bn.move_mean(force_avg, nsmooth)
 
-        plt.plot(self.t, force_avg, label="avg force shifted")
+        plt.plot(self.t, -force_avg, label="avg force shifted")
+        plt.xlim(-0.05, 2.0)
+        plt.ylim(0, None)
         plt.legend()
         plt.show()
 
@@ -338,7 +429,7 @@ class solveCFC:
         plt.plot(-self.specimen_strain, -self.specimen_stress, label="average specimen stress/strain")
         plt.legend()
         plt.ylim(0, None)
-        plt.show()
+        #plt.show()
         
         
     
